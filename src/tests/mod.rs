98b44aadc596d8dad45c507c2b06c8d188bc52f8
Stelios Daveas <sdaveas@gmail.com>
2021-04-22 21:26:24 +0300

    Create a test case struct

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -25,21 +25,59 @@ use tracing_test::traced_test;
 #[cfg(feature = "malicious")]
 use tofn::protocol::gg20::sign::malicious::MaliciousType::{self, *};
 
-#[cfg(not(feature = "malicious"))]
-type TestCase = (usize, Vec<u32>, usize, Vec<usize>);
-#[cfg(feature = "malicious")]
-type TestCase = (usize, Vec<u32>, usize, Vec<usize>, Vec<MaliciousType>);
+struct TestCase {
+    uid_count: usize,
+    share_counts: Vec<u32>,
+    threshold: usize,
+    signer_indices: Vec<usize>,
+    #[cfg(feature = "malicious")]
+    malicious_types: Vec<MaliciousType>,
+}
+
+impl TestCase {
+    #[cfg(not(feature = "malicious"))]
+    fn new(
+        uid_count: usize,
+        share_counts: Vec<u32>,
+        threshold: usize,
+        signer_indices: Vec<usize>,
+    ) -> TestCase {
+        TestCase {
+            uid_count,
+            share_counts,
+            threshold,
+            signer_indices,
+        }
+    }
+
+    #[cfg(feature = "malicious")]
+    fn new(
+        uid_count: usize,
+        share_counts: Vec<u32>,
+        threshold: usize,
+        signer_indices: Vec<usize>,
+        malicious_types: Vec<MaliciousType>,
+    ) -> TestCase {
+        TestCase {
+            uid_count,
+            share_counts,
+            threshold,
+            signer_indices,
+            malicious_types,
+        }
+    }
+}
 
 #[cfg(not(feature = "malicious"))]
 lazy_static::lazy_static! {
     static ref MSG_TO_SIGN: Vec<u8> = vec![42];
     // (number of uids, count of shares per uid, threshold, indices of sign participants)
     static ref TEST_CASES: Vec<TestCase> = vec![
-        (4, vec![], 0, vec![0,1,2,3]),              // should initialize share_counts into [1,1,1,1,1]
-        (5, vec![1,1,1,1,1], 3, vec![1,4,2,3]),     // 1 share per uid
-        (5, vec![1,2,1,3,2], 6, vec![1,4,2,3]),     // multiple shares per uid
-        (1, vec![1], 0, vec![0]),                   // trivial case
-        (5, vec![1,2,3,4,20], 27, vec![0, 1, 4, 3, 2]), // Create a malicious party
+        TestCase::new(4, vec![], 0, vec![0,1,2,3]),              // should initialize share_counts into [1,1,1,1,1]
+        TestCase::new(5, vec![1,1,1,1,1], 3, vec![1,4,2,3]),     // 1 share per uid
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![1,4,2,3]),     // multiple shares per uid
+        TestCase::new(1, vec![1], 0, vec![0]),                   // trivial case
+        TestCase::new(5, vec![1,2,3,4,20], 27, vec![0, 1, 4, 3, 2]), // Create a malicious party
     ];
 }
 
@@ -48,22 +86,22 @@ lazy_static::lazy_static! {
     static ref MSG_TO_SIGN: Vec<u8> = vec![42];
     // (number of uids, count of shares per uid, threshold, indices of sign participants, malicious types)
     static ref TEST_CASES: Vec<TestCase> = vec![
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest; 5]),    // only honest
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R1BadProof{victim:0}]),  // R1BadProof
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R1FalseAccusation{victim:0}]),  // R1FalseAccusation
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2BadMta{victim:0}]),  // R2BadMta
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2BadMtaWc{victim:0}]),  // R2BadMtaWc
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2FalseAccusationMta{victim:0}]),  // R2FalseAccusationMta
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2FalseAccusationMtaWc{victim:0}]),  // R2FalseAccusationMtaWc
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R3BadProof]),  // R3BadProof
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R3FalseAccusation{victim:0}]),  // R3FalseAccusation
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R4BadReveal]),  // R4BadReveal
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R4FalseAccusation{victim:0}]),  // R4FalseAccusation
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R5BadProof{victim:0}]),  // R5BadProof
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R5FalseAccusation{victim:0}]),  // R5FalseAccusation
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R6BadProof]),  // R6BadProof
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R6FalseAccusation{victim:0}]),  // R6FalseAccusation
-        (5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R7BadSigSummand]),  // R7BadSigSummand
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest; 5]),    // only honest
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R1BadProof{victim:0}]),  // R1BadProof
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R1FalseAccusation{victim:0}]),  // R1FalseAccusation
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2BadMta{victim:0}]),  // R2BadMta
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2BadMtaWc{victim:0}]),  // R2BadMtaWc
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2FalseAccusationMta{victim:0}]),  // R2FalseAccusationMta
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R2FalseAccusationMtaWc{victim:0}]),  // R2FalseAccusationMtaWc
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R3BadProof]),  // R3BadProof
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R3FalseAccusation{victim:0}]),  // R3FalseAccusation
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R4BadReveal]),  // R4BadReveal
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R4FalseAccusation{victim:0}]),  // R4FalseAccusation
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R5BadProof{victim:0}]),  // R5BadProof
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R5FalseAccusation{victim:0}]),  // R5FalseAccusation
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R6BadProof]),  // R6BadProof
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R6FalseAccusation{victim:0}]),  // R6FalseAccusation
+        TestCase::new(5, vec![1,2,1,3,2], 6, vec![0,1,2,3,4], vec![Honest, Honest, Honest, Honest, R7BadSigSummand]),  // R7BadSigSummand
         // TODO add more complex tests for malicious behaviours
     ];
 }
@@ -97,15 +135,15 @@ async fn basic_keygen_and_sign() {
 
     // for (uid_count, party_share_counts, threshold, sign_participant_indices, malicious_types) in
     for test_case in TEST_CASES.iter() {
-        let uid_count = test_case.0;
-        let party_share_counts = &test_case.1;
-        let threshold = test_case.2;
-        let sign_participant_indices = &test_case.3;
+        let uid_count = test_case.uid_count;
+        let party_share_counts = &test_case.share_counts;
+        let threshold = test_case.threshold;
+        let sign_participant_indices = &test_case.signer_indices;
 
         // get malicious types only when we are in malicious mode
         #[cfg(feature = "malicious")]
         let malicious_types = {
-            let malicious_types = test_case.4.clone();
+            let malicious_types = test_case.malicious_types.clone();
             match malicious_types.len() {
                 0 => vec![Honest; uid_count],
                 _ => malicious_types,
@@ -157,15 +195,15 @@ async fn restart_one_party() {
     let dir = testdir!();
 
     for test_case in TEST_CASES.iter() {
-        let uid_count = test_case.0;
-        let party_share_counts = test_case.1.clone();
-        let threshold = test_case.2;
-        let sign_participant_indices = test_case.3.clone();
+        let uid_count = test_case.uid_count;
+        let party_share_counts = &test_case.share_counts;
+        let threshold = test_case.threshold;
+        let sign_participant_indices = &test_case.signer_indices;
 
         // get malicious types only when we are in malicious mode
         #[cfg(feature = "malicious")]
         let malicious_types = {
-            let malicious_types = test_case.4.clone();
+            let malicious_types = test_case.malicious_types.clone();
             match malicious_types.len() {
                 0 => vec![Honest; uid_count],
                 _ => malicious_types,
@@ -188,7 +226,7 @@ async fn restart_one_party() {
         let parties = execute_keygen(
             parties,
             &party_uids,
-            &party_share_counts,
+            party_share_counts,
             new_key_uid,
             threshold,
         )
