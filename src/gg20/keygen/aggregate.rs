87300ab4eac5fcd2f1e6f4bbcc330390e8e74459
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-07-12 20:56:18 +0300

    refactor/keygen (#107)
    * Create keygen mod and put all related functions inside
    
    * Wrap process keygen init in separate fn
    
    * Documentation and testing for keygen init mod
    
    * move keygen execute to a separate module
    
    * WIP: move aggregator to separate module
    
    More work is needed here:
    * clean up huge function
    * remove unwraps
    
    * clean up imports
    
    * Refactor aggregate module
    
    * add documentation at types module
    
    * update doc

---
+++ src/gg20/keygen/aggregate.rs
@@ -0,0 +1,143 @@
+//! This module handles the aggregation and keygen results.
+//! When all keygen threads finish, we aggregate their results and retrieve:
+//!  1. the public key - must be the same across all results; stored in KvStore
+//!  2. all secret share data - data used to allow parties to participate to future Signs; stored in KvStore
+//!  3. all secret share recovery info - information used to allow client to issue secret share recovery in case of data loss; sent to client
+
+use tofn::protocol::gg20::{keygen::KeygenOutput, SecretKeyShare};
+
+use super::{
+    proto::{self, message_out::keygen_result},
+    types::KeygenInitSanitized,
+    Gg20Service, PartyInfo,
+};
+use crate::{kv_manager::KeyReservation, TofndError};
+
+// tonic cruft
+use tokio::sync::{
+    mpsc,
+    oneshot::{self, Receiver},
+};
+use tonic::Status;
+
+impl Gg20Service {
+    /// aggregate messages from all keygen workers, create a record and insert it in the KVStore
+    pub(super) async fn aggregate_messages(
+        &mut self,
+        aggregator_receivers: Vec<oneshot::Receiver<Result<KeygenOutput, TofndError>>>,
+        stream_out_sender: &mut mpsc::UnboundedSender<Result<proto::MessageOut, Status>>,
+        key_uid_reservation: KeyReservation,
+        keygen_init: KeygenInitSanitized,
+    ) -> Result<(), TofndError> {
+        // wait all keygen threads and aggregate results
+        let keygen_outputs = match Self::aggregate_keygen_outputs(aggregator_receivers).await {
+            Ok(keygen_outputs) => keygen_outputs,
+            Err(err) => {
+                self.shares_kv.unreserve_key(key_uid_reservation).await;
+                return Err(From::from(format!(
+                    "Error at Keygen output aggregation. Unreserving key {}",
+                    err
+                )));
+            }
+        };
+
+        // try to process keygen outputs
+        let (pub_key, secret_key_shares) =
+            Self::process_keygen_outputs(&keygen_init, keygen_outputs, stream_out_sender)?;
+
+        // try to retrieve recovery info from all shares
+        let mut share_recovery_infos = vec![];
+        for secret_key_share in secret_key_shares.iter() {
+            share_recovery_infos.push(bincode::serialize(&secret_key_share.recovery_info())?);
+        }
+
+        // combine responses from all keygen threads to a single struct
+        let kv_data = PartyInfo::get_party_info(
+            secret_key_shares,
+            keygen_init.party_uids.clone(),
+            keygen_init.party_share_counts.clone(),
+            keygen_init.my_index,
+        );
+
+        // try to put data inside kv store
+        self.shares_kv.put(key_uid_reservation, kv_data).await?;
+
+        // try to send result
+        Ok(
+            stream_out_sender.send(Ok(proto::MessageOut::new_keygen_result(
+                &keygen_init.party_uids,
+                &keygen_init.party_share_counts,
+                Ok(keygen_result::KeygenOutput {
+                    pub_key,
+                    share_recovery_infos,
+                }),
+            )))?,
+        )
+    }
+
+    /// iterate all keygen outputs, and return data that need to be permenantly stored
+    /// from all keygen outputs we need to extract the common public key and each secret key share
+    fn process_keygen_outputs(
+        keygen_init: &KeygenInitSanitized,
+        keygen_outputs: Vec<KeygenOutput>,
+        stream_out_sender: &mut mpsc::UnboundedSender<Result<proto::MessageOut, Status>>,
+    ) -> Result<(Vec<u8>, Vec<SecretKeyShare>), TofndError> {
+        // all shares must return the same public key. To ease pub key uniqueness, we use a hasmap
+        let mut pub_key_map = std::collections::HashMap::new();
+
+        // prepare a vec to hold secret key shares for each share
+        let mut secret_key_shares = Vec::with_capacity(keygen_outputs.len());
+
+        for keygen_output in keygen_outputs {
+            match keygen_output {
+                // if keygen output was ok, hold `secret_key_share` in a vec and add public key to a hasmap
+                Ok(secret_key_share) => {
+                    let pub_key = secret_key_share.group.pubkey_bytes();
+                    secret_key_shares.push(secret_key_share);
+                    // hashmap [pub key -> count] with default value 0
+                    *pub_key_map.entry(pub_key).or_insert(0) += 1;
+                }
+                // if keygen output was an error, send discovered criminals to client and exit
+                Err(crimes) => {
+                    // send crimes and exit with an error
+                    stream_out_sender.send(Ok(proto::MessageOut::new_keygen_result(
+                        &keygen_init.party_uids,
+                        &keygen_init.party_share_counts,
+                        Err(crimes.clone()),
+                    )))?;
+                    return Err(From::from(format!("Crimes found: {:?}", crimes)));
+                }
+            }
+        }
+
+        // assert that all shares returned the same public key
+        let pub_key = Self::validate_pubkey(pub_key_map)?;
+
+        Ok((pub_key, secret_key_shares))
+    }
+
+    /// wait all keygen threads and get keygen outputs
+    async fn aggregate_keygen_outputs(
+        aggregator_receivers: Vec<Receiver<Result<KeygenOutput, TofndError>>>,
+    ) -> Result<Vec<KeygenOutput>, TofndError> {
+        let mut keygen_outputs = Vec::with_capacity(aggregator_receivers.len());
+        for aggregator in aggregator_receivers {
+            let res = aggregator.await??;
+            keygen_outputs.push(res);
+        }
+        Ok(keygen_outputs)
+    }
+
+    /// check that all shares returned the same public key
+    fn validate_pubkey(
+        pub_key_map: std::collections::HashMap<Vec<u8>, i32>,
+    ) -> Result<Vec<u8>, TofndError> {
+        if pub_key_map.len() != 1 {
+            return Err(From::from(format!(
+                "Shares returned different public key {:?}",
+                pub_key_map
+            )));
+        }
+        Ok(pub_key_map.keys().last().unwrap().to_owned())
+    }
+}
---
+++ src/gg20/keygen/execute.rs
@@ -0,0 +1,94 @@
+//! This module executes the keygen protocol
+//! On success it returns [KeygenOutput]. A successful [Keygen] can produce either an Ok(SecretKeyShare) of an Err(Vec<Vec<Crime>>).
+//! On failure it returns [TofndError] if [Keygen] cannot be instantiated.
+
+use super::{proto, protocol, types::Context, Gg20Service, ProtocolCommunication};
+use crate::TofndError;
+use tofn::protocol::gg20::keygen::{Keygen, KeygenOutput};
+
+// logging
+use tracing::{info, span, warn, Level, Span};
+
+impl Gg20Service {
+    /// execute keygen protocol and return result
+    pub(super) async fn execute_keygen(
+        &self,
+        chans: ProtocolCommunication<
+            Option<proto::TrafficIn>,
+            Result<proto::MessageOut, tonic::Status>,
+        >,
+        ctx: &Context,
+        execute_span: Span,
+    ) -> Result<KeygenOutput, TofndError> {
+        // create keygen with context
+        let mut keygen = Keygen::new(
+            ctx.total_share_count(),
+            ctx.threshold,
+            ctx.tofn_index(),
+            &self.seed().await?,
+            &ctx.nonce(),
+        )?;
+        // set up behaviour if we run in malicious mode
+        #[cfg(feature = "malicious")]
+        keygen.set_behaviour(self.keygen_behaviour.clone());
+
+        // execute protocol and wait for completion
+        let protocol_result = protocol::execute_protocol(
+            &mut keygen,
+            chans,
+            &ctx.uids,
+            &ctx.share_counts,
+            execute_span.clone(),
+        )
+        .await;
+
+        // return processed result
+        Ok(Self::process_result(
+            keygen,
+            protocol_result,
+            execute_span.clone(),
+        )?)
+    }
+
+    /// constructs response from given protocol's state and result
+    fn process_result(
+        protocol_state: Keygen,
+        protocol_result: Result<(), TofndError>,
+        execute_span: Span,
+    ) -> Result<KeygenOutput, TofndError> {
+        let result_span = span!(parent: &execute_span, Level::INFO, "result");
+        let _enter = result_span.enter();
+
+        let keygen_output = match protocol_result {
+            // if protocol result is ok, return keygen output
+            Ok(()) => {
+                info!("Keygen completed successfully");
+                protocol_state
+                    .clone_output()
+                    .ok_or("keygen output is `None`")?
+            }
+            // if protocol result was an error, check for disrupting parties
+            Err(err) => match protocol_state.found_disrupting() {
+                // if disrupting parties were found, return keygen output
+                true => {
+                    warn!("Party failed due to deserialization error: {}", err);
+                    protocol_state
+                        .clone_output()
+                        .ok_or("keygen output is `None`")?
+                }
+                // if no disrupting parties were found, return the waiting on parties
+                false => {
+                    let waiting_on = protocol_state.waiting_on();
+                    warn!(
+                        "Protocol ended prematurely: while I was waiting on {:?} {}",
+                        waiting_on, err
+                    );
+                    Err(waiting_on)
+                }
+            },
+        };
+
+        // return result
+        Ok(keygen_output)
+    }
+}
