99c6ba5915f70bbea8f3631f6fb84e035bcb859f
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-07-20 23:29:25 +0300

    remove old keygen test cases

--- src/tests/malicious/keygen_test_cases.rs
+++ src/tests/malicious/keygen_test_cases.rs
@@ -39,12 +39,6 @@ async fn malicious_disrupt_cases() {
     run_test_cases(&disrupt_cases()).await;
 }
 
-// #[traced_test]
-// #[tokio::test]
-// async fn malicious_spoof_cases() {
-//     run_test_cases(&spoof_cases()).await;
-// }
-
 #[derive(Clone, Debug)]
 pub(crate) struct Spoof {
     pub(crate) index: usize,
@@ -216,75 +210,3 @@ fn disrupt_cases() -> Vec<TestCase> {
         })
         .collect()
 }
-
-// fn timeout_cases() -> Vec<TestCase> {
-//     use MsgType::*;
-//     let stallers = MsgType::iter()
-//         .filter(|msg_type| matches!(msg_type, R1Bcast | R2Bcast | R2P2p { to: _ } | R3Bcast,)) // don't match fail types
-//         .map(|msg_type| Staller { msg_type })
-//         .collect::<Vec<Behaviour>>();
-
-//     // staller always targets party 0
-//     stallers
-//         .iter()
-//         .map(|staller| {
-//             TestCase::new_malicious_keygen(4, vec![1, 1, 1, 1], 2, vec![Honest, Honest, Honest])
-//         })
-//         .collect()
-// }
-
-// fn disrupt_cases() -> Vec<TestCase> {
-//     use MsgType::*;
-//     let disrupters = MsgType::iter()
-//         .filter(|msg_type| matches!(msg_type, R1Bcast | R2Bcast | R2P2p { to: _ } | R3Bcast,)) // don't match fail types
-//         // .filter(|msg_type| matches!(msg_type, R1Bcast)) // don't match fail types
-//         .map(|msg_type| DisruptingSender { msg_type })
-//         .collect::<Vec<Behaviour>>();
-
-//     // disrupter always targets party 0
-//     disrupters
-//         .iter()
-//         .map(|disrupter| {
-//             TestCase::new_malicious_keygen(
-//                 5,
-//                 vec![1, 2, 1, 1, 1],
-//                 3,
-//                 vec![
-//                     Keygener::new(Honest, vec![]),
-//                     Keygener::new(disrupter.clone(), vec![to_crime(&disrupter)]),
-//                     Keygener::new(Honest, vec![]),
-//                     Keygener::new(Honest, vec![]),
-//                     Keygener::new(Honest, vec![]),
-//                 ],
-//             )
-//         })
-//         .collect()
-// }
-
-// fn spoof_cases() -> Vec<TestCase> {
-//     use Status::*;
-//     let victim = 0;
-//     let spoofers = Status::iter()
-//         .filter(|status| matches!(status, R1 | R2 | R3)) // don't match fail types
-//         .map(|status| UnauthenticatedSender { victim, status })
-//         .collect::<Vec<Behaviour>>();
-
-//     // spoofer always targets party 0
-//     spoofers
-//         .iter()
-//         .map(|spoofer| {
-//             TestCase::new_malicious_keygen(
-//                 5,
-//                 vec![1, 1, 1, 1, 1],
-//                 3,
-//                 vec![
-//                     Keygener::new(Honest, vec![]),
-//                     Keygener::new(spoofer.clone(), vec![to_crime(&spoofer)]),
-//                     Keygener::new(Honest, vec![]),
-//                     Keygener::new(Honest, vec![]),
-//                     Keygener::new(Honest, vec![]),
-//                 ],
-//             )
-//         })
-//         .collect()
-// }
