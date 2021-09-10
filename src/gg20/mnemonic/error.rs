8cc33e8997339c977f1450d7531eb1a4070d4e40
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-09-10 15:00:05 +0300

    chore: tidy error handling logs (#183)
    * Fix error display
    
    * Add comments on why we are using thiserror in some mods

--- src/gg20/mnemonic/error.rs
+++ src/gg20/mnemonic/error.rs
@@ -1,3 +1,13 @@
+//! Custom error types for [mnemonic].
+
+/// Note: While tofnd generally uses the [anyhow] crate for error handling, we
+/// use the [thiserror] crate here for two reasons:
+/// 1. Mnemonic errors can be potentially consumed by the caller of tofnd, so an
+/// analytical display of errors might be helpful in the future
+/// 2. This can be used as an example on how analytical error handling can be
+/// incorporated in other modules
+/// For more info, see discussion in https://github.com/axelarnetwork/tofnd/issues/28
+
 pub(super) mod bip39 {
     #[derive(thiserror::Error, Debug)]
     pub enum Bip39Error {
@@ -27,7 +37,7 @@ pub(super) mod mnemonic {
         FileIoErr(#[from] super::file_io::FileIoError),
         #[error("KvStore error: {0}")]
         KvErr(#[from] crate::kv_manager::error::KvError),
-        #[error("Bip39 error {0}")]
+        #[error("Bip39 error: {0}")]
         Bip39Error(#[from] super::bip39::Bip39Error),
         #[error("Failed to convert to SecretRecoveryKey")]
         IntoSecretRecoveryKey(#[from] std::array::TryFromSliceError),
@@ -36,15 +46,15 @@ pub(super) mod mnemonic {
 
     #[derive(thiserror::Error, Debug)]
     pub enum MnemonicError {
-        #[error("Command not found {0}")]
+        #[error("Command not found: {0}")]
         WrongCommand(String),
-        #[error("Cannot create mnemonic {0}")]
+        #[error("Cannot create mnemonic: {0}")]
         CreateErr(InnerMnemonicError),
-        #[error("Cannot import mnemonic {0}")]
+        #[error("Cannot import mnemonic: {0}")]
         ImportErr(InnerMnemonicError),
-        #[error("Cannot export mnemonic {0}")]
+        #[error("Cannot export mnemonic: {0}")]
         ExportErr(InnerMnemonicError),
-        #[error("Cannot update mnemonic {0}")]
+        #[error("Cannot update mnemonic: {0}")]
         UpdateErr(InnerMnemonicError),
     }
     pub type MnemonicResult<Success> = Result<Success, MnemonicError>;
