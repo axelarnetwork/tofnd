5106230a46b68eea2197153af1c730e186b78580
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-09-10 14:12:44 +0300

    Fix error display

--- src/gg20/mnemonic/error.rs
+++ src/gg20/mnemonic/error.rs
@@ -27,7 +27,7 @@ pub(super) mod mnemonic {
         FileIoErr(#[from] super::file_io::FileIoError),
         #[error("KvStore error: {0}")]
         KvErr(#[from] crate::kv_manager::error::KvError),
-        #[error("Bip39 error {0}")]
+        #[error("Bip39 error: {0}")]
         Bip39Error(#[from] super::bip39::Bip39Error),
         #[error("Failed to convert to SecretRecoveryKey")]
         IntoSecretRecoveryKey(#[from] std::array::TryFromSliceError),
@@ -36,15 +36,15 @@ pub(super) mod mnemonic {
 
     #[derive(thiserror::Error, Debug)]
     pub enum MnemonicError {
-        #[error("Command not found {0}")]
+        #[error("Command not found: {0}")]
         WrongCommand(String),
-        #[error("Cannot create mnemonic {0}")]
+        #[error("Cannot create mnemonic: {0}")]
         CreateErr(InnerMnemonicError),
-        #[error("Cannot import mnemonic {0}")]
+        #[error("Cannot import mnemonic: {0}")]
         ImportErr(InnerMnemonicError),
-        #[error("Cannot export mnemonic {0}")]
+        #[error("Cannot export mnemonic: {0}")]
         ExportErr(InnerMnemonicError),
-        #[error("Cannot update mnemonic {0}")]
+        #[error("Cannot update mnemonic: {0}")]
         UpdateErr(InnerMnemonicError),
     }
     pub type MnemonicResult<Success> = Result<Success, MnemonicError>;
