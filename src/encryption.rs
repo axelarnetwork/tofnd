1a98165c93bb486c210a05f50c10d49bc67d80f7
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-09-14 13:43:50 +0300

    Tidy up encryption mod

--- src/encryption.rs
+++ src/encryption.rs
@@ -1,10 +1,8 @@
-//! Encryption module. ChaCha20 is used to encrypt the kvstore
+//! Encryption module. [chacha20::ChaCha20] is used to encrypt the kvstore
 
-/// Password Error
+/// Encryption errors
 #[derive(thiserror::Error, Debug)]
 pub enum EncryptionErr {
-    #[error("Password too large: Max length is 32, got {0}")]
-    TooLarge(usize),
     #[error("Read error: {0}")]
     Read(String),
     #[error("Cipher error: {0}")]
@@ -13,53 +11,51 @@ pub enum EncryptionErr {
 pub type EncryptionResult<Success> = Result<Success, EncryptionErr>;
 use EncryptionErr::*;
 
-// zeroize Entropy and Password
-use zeroize::Zeroize;
-
-/// use sha3 to hash
+/// use [sha3_hash] as hashing algorithm
 type HashAlgo = sha3_hash::Hash;
 
+// zeroize Entropy and Password
 use serde::{Deserialize, Serialize};
+use zeroize::Zeroize;
 
-/// Mnemonic type needs to be known globaly to create/access the mnemonic kv store
+/// store mnemonic entropy safely
 #[derive(Zeroize, Debug, Clone, Serialize, Deserialize)]
 #[zeroize(drop)]
 pub struct Entropy(pub Vec<u8>);
 
-/// Store passwords strings safely
+/// store strings safely
 #[derive(Zeroize, Clone)]
 #[zeroize(drop)]
 pub struct Password(pub String);
 
+/// Defines how the password for the kv-store will be retrieved
 pub enum PasswordMethod {
     #[cfg(test)]
-    TestPassword, // use default password
+    TestPassword, // use test password
     Prompt, // prompt user for password
 }
-
-/// Defines how the password will be retireved
 impl PasswordMethod {
     pub fn get(&self) -> EncryptionResult<Password> {
         match self {
             #[cfg(test)]
-            Self::TestPassword => Ok(Password(DEFAULT_PASSWORD.to_owned())),
+            Self::TestPassword => Ok(Password(DEFAULT_PASSWORD.to_owned())), // test password
             Self::Prompt => prompt("Type password", PASSWORD_LENGTH),
         }
     }
 }
 
-/// default nonce is 12 digits because encrypted_sled uses ChaCha20<R20, C32>
-const DEFAULT_NONCE: &str = "123456789012"; // 12 digits
-/// Password should be 32 digits because encrypted_sled uses ChaCha20<R20, C32>
+/// Password should be 32 digits because encrypted_sled uses [chacha20::ChaCha20<R20, C32>]
 const PASSWORD_LENGTH: usize = 32;
+/// default nonce is 12 digits because encrypted_sled uses [chacha20::ChaCha20<R20, C32>]
+const DEFAULT_NONCE: &str = "123456789012"; // 12 digits
 
-/// alias for encryption cipher
+/// alias for encryption cipher to be used by [encrypted_sled]
 type ChaCha20EncryptionCipher = encrypted_sled::EncryptionCipher<chacha20::ChaCha20>;
 
-/// alias for encrypted kv database
+/// alias for encrypted_sled database
 pub type EncryptedDb = encrypted_sled::Db<ChaCha20EncryptionCipher>;
 
-/// prompt user for password. The password is hashed using
+/// prompt user for password. The password is hashed using [HashAlgo].
 pub(super) fn prompt(msg: &str, length: usize) -> EncryptionResult<Password> {
     println!("{}", msg);
     // prompt user for a password and hash it
@@ -83,6 +79,7 @@ pub(super) fn encryption_cipher(password: Password) -> EncryptionResult<ChaCha20
     .map_err(|err| Cipher(err.to_string()))
 }
 
+/// we don't want tests to prompt for password
 #[cfg(test)]
 /// default password is 32 digits because encrypted_sled uses ChaCha20<R20, C32>
 const DEFAULT_PASSWORD: &str = "12345678901234567890123456789012"; // 32 digits
