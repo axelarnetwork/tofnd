0c535202551ef6f1a667cdfc4b0fc9bfef3535cf
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-09-23 23:10:31 +0300

    Print an error if we can't send error status code to client
    We can't handle errors in tokio threads and we have already returned
    from the gRPC.

--- src/gg20/mod.rs
+++ src/gg20/mod.rs
@@ -97,7 +97,10 @@ impl proto::gg20_server::Gg20 for service::Gg20Service {
             // can't return an error from a spawned thread
             if let Err(e) = gg20.handle_keygen(stream_in, msg_sender.clone(), s).await {
                 error!("keygen failure: {:?}", e.to_string());
-                let _ = msg_sender.send(Err(Status::internal(e.to_string())));
+                // we can't handle errors in tokio threads. Log error if we are unable to send the status code to client.
+                if let Err(e) = msg_sender.send(Err(Status::internal(e.to_string()))) {
+                    error!("could not send error to client: {}", e.to_string());
+                }
                 return;
             }
         });
@@ -122,7 +125,10 @@ impl proto::gg20_server::Gg20 for service::Gg20Service {
             // can't return an error from a spawned thread
             if let Err(e) = gg20.handle_sign(stream, msg_sender.clone(), s).await {
                 error!("sign failure: {:?}", e.to_string());
-                let _ = msg_sender.send(Err(Status::internal(e.to_string())));
+                // we can't handle errors in tokio threads. Log error if we are unable to send the status code to client.
+                if let Err(e) = msg_sender.send(Err(Status::internal(e.to_string()))) {
+                    error!("could not send error to client: {}", e.to_string());
+                }
                 return;
             }
         });
