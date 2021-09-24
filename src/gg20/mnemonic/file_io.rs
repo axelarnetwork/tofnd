2c0a48ece1f6f3d33db15e7a29f18ee102546b0f
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-09-24 17:09:41 +0300

    Remove read from file operations

--- src/gg20/mnemonic/file_io.rs
+++ src/gg20/mnemonic/file_io.rs
@@ -1,14 +1,11 @@
 //! This module handles IO to files. More specifically, writing and reading passphrases.
 
-use std::{
-    io::{Read, Write},
-    path::PathBuf,
-};
+use std::{io::Write, path::PathBuf};
 
 use tracing::info;
 
 use super::{bip39_bindings::bip39_from_entropy, error::file_io::FileIoError::Exists};
-use crate::gg20::types::{Entropy, Password};
+use crate::gg20::types::Entropy;
 
 /// name of export file
 const EXPORT_FILE: &str = "export";
@@ -48,44 +45,31 @@ impl FileIo {
         info!("Mnemonic written in file {:?}", filepath);
         Ok(())
     }
-
-    /// Returns the phrase from a file
-    pub(super) fn phrase_from_file(&self, filename: &str) -> FileIoResult<Password> {
-        let mut filepath = self.path.clone();
-        filepath.push(filename);
-        let mut file = std::fs::File::open(filepath)?;
-        let mut mnemonic_phrase = String::new();
-        // if read_to_string fails, we don't need to worry about zeroizing mnemonic phrase; we never got it
-        file.read_to_string(&mut mnemonic_phrase)?;
-        Ok(Password(mnemonic_phrase))
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::gg20::mnemonic::bip39_bindings::{bip39_new_w24, tests::bip39_to_phrase};
+    use std::io::Read;
     use testdir::testdir;
     use tracing_test::traced_test;
 
     #[traced_test]
     #[test]
-    fn test_write_read() {
-        let io = FileIo { path: testdir!() };
+    fn test_write() {
         let entropy = bip39_new_w24();
-        let entropy_copy = entropy.clone();
-        let filepath = io.filepath();
-        io.entropy_to_file(entropy).unwrap();
-        let filename = filepath.file_name().unwrap().to_str().unwrap();
-        let file_content = io.phrase_from_file(filename).unwrap();
-        let expected_content = bip39_to_phrase(entropy_copy).unwrap();
-        assert_eq!(file_content.0, expected_content.0);
-    }
 
-    #[test]
-    fn test_read() {
         let io = FileIo { path: testdir!() };
-        let file_content = io.phrase_from_file("non-existing-file");
-        assert!(file_content.is_err());
+        let filepath = io.filepath();
+        io.entropy_to_file(entropy.clone()).unwrap();
+        let expected_content = bip39_to_phrase(entropy).unwrap();
+
+        let mut file = std::fs::File::open(filepath).unwrap();
+        let mut file_phrase = String::new();
+        file.read_to_string(&mut file_phrase).unwrap();
+        let file_content = file_phrase;
+
+        assert_eq!(file_content, expected_content.0);
     }
 }
