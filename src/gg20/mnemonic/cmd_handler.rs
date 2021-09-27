dd3769c005e20751917052aaa8fc742062be0ac1
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-09-27 19:33:58 +0300

    Rename assert_not_exported->check_if_not_exported

--- src/gg20/mnemonic/cmd_handler.rs
+++ src/gg20/mnemonic/cmd_handler.rs
@@ -79,7 +79,7 @@ impl Gg20Service {
     async fn handle_existing(&self) -> InnerMnemonicResult<()> {
         // if there is an exported mnemonic, raise an error and don't start the daemon.
         // we do this to prevent users from accidentally leave their mnemonic on disk in plain text
-        self.io.assert_not_exported()?;
+        self.io.check_if_not_exported()?;
 
         // try to get mnemonic from kv-store
         match self.mnemonic_kv.exists(MNEMONIC_KEY).await? {
