c048f9cd363a1ed97e5bc14e7ef35b14478a0bdd
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-10-19 18:59:27 +0300

    small changes in handle keygen

--- src/multisig/keygen.rs
+++ src/multisig/keygen.rs
@@ -6,10 +6,10 @@ use tofn::{ecdsa::keygen, sdk::api::serialize};
 
 impl MultisigService {
     pub(super) async fn handle_keygen(&self, request: KeygenRequest) -> TofndResult<Vec<u8>> {
-        let session_nonce = request.key_id;
-        let secret_key_share = self.kv_manager.seed().await?;
+        let session_nonce = request.key_uid;
+        let secret_recovery_key = self.kv_manager.seed().await?;
 
-        let key_pair = keygen(&secret_key_share, session_nonce.as_bytes())
+        let key_pair = keygen(&secret_recovery_key, session_nonce.as_bytes())
             .map_err(|_| anyhow!("Cannot generate keypair"))?;
 
         let reservation = self.kv_manager.kv().reserve_key(session_nonce).await?;
