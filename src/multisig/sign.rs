937266c9daee38be3eb30aa99a7e4c208cc9e01e
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-11-29 17:04:16 +0200

    add comments and edit error msgs
    as per @ggutoski's indications

--- src/multisig/sign.rs
+++ src/multisig/sign.rs
@@ -7,16 +7,17 @@ use tofn::ecdsa::{keygen, sign};
 
 impl MultisigService {
     pub(super) async fn handle_sign(&self, request: &SignRequest) -> TofndResult<Vec<u8>> {
+        // re-generate secret key from seed, then sign
         let secret_recovery_key = self.kv_manager.seed().await?;
 
         let key_pair = keygen(&secret_recovery_key, request.key_uid.as_bytes())
-            .map_err(|_| anyhow!("Cannot generate keypair"))?;
+            .map_err(|_| anyhow!("key re-generation failed"))?;
 
         let signature = sign(
             key_pair.signing_key(),
             &request.msg_to_sign.as_slice().try_into()?,
         )
-        .map_err(|_| anyhow!("Cannot generate keypair"))?;
+        .map_err(|_| anyhow!("sign failed"))?;
 
         Ok(signature)
     }
