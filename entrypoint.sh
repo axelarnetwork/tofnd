44665f60f7c2458e212b1e4d177ecd8ce84ecd54
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-12-30 07:03:20 +0200

    chore: make docker create and export cmds equivalent to binary cmds (#254)
    * make docker create and export cmds equivalent to binary cmds
    
    * [no ci] tweak comments
    
    Co-authored-by: Gus Gutoski <gus.gutoski@protonmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 OK=0
 ERR=1
 
-# create: create a new mnemonic, export it to a file under the name "import" and continue
+# create: create a new mnemonic and export it to $EXPORT_PATH
 create_mnemonic() {
     echo "Creating mnemonic ..."
 
@@ -15,11 +15,11 @@ create_mnemonic() {
         return $ERR
     fi
 
-    (echo ${PASSWORD} | tofnd ${ARGS} -m create) && mv $EXPORT_PATH $IMPORT_PATH && echo "... ok" && return $OK
+    (echo ${PASSWORD} | tofnd ${ARGS} -m create) && echo "... ok" && return $OK
     return $ERR
 }
 
-# import: import a mnemonic from $IMPORT_PATH and continue
+# import: import a mnemonic from $IMPORT_PATH
 import_mnemonic() {
     echo "Importing mnemonic ..."
 
@@ -45,10 +45,10 @@ import_mnemonic() {
     return $OK
 }
 
-# export: export the mnemonic to $EXPORT_PATH, move it to $IMPORT_PATH and exit
+# export: export the mnemonic to $EXPORT_PATH
 export_mnemonic() {
     echo "Exporting mnemonic ..."
-    echo ${PASSWORD} | tofnd ${ARGS} -m export && mv $EXPORT_PATH $IMPORT_PATH || return $ERR
+    echo ${PASSWORD} | tofnd ${ARGS} -m export || return $ERR
     echo "... ok"
     return $OK
 }
@@ -78,9 +78,10 @@ if [ -n "${MNEMONIC_CMD}" ]; then \
     case ${MNEMONIC_CMD} in
         # auto: try to set up tofnd and then spin up tofnd with the existing mnemonic.
         # Order of set up: 1) import mnemonic, 2) create mnemonic.
+        # If 2) then move the mnemonic to $IMPORT_PATH so that tofnd will not complain
         auto)
             echo "Trying import" && import_mnemonic \
-            || (echo "... skipping. Trying to create" && create_mnemonic) \
+            || (echo "... skipping. Trying to create" && create_mnemonic && mv $EXPORT_PATH $IMPORT_PATH) \
             || echo "... skipping"
             ;;
 
